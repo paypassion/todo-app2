63d9a89288a5874522da0878ed6e44fa9a1a3c74
paypassion <zion4596@gmail.com>
2024-03-05 20:36:01 +0900

    first commit

---
+++ demo.js
@@ -0,0 +1,21 @@
+// 데이터 타입
+// string 문자열 "가나다"
+// number 1 10
+// booleadn
+true;
+false;
+// null
+null;
+
+// undefind
+undefined;
+
+// 변수
+height = 180;
+christ = true;
+console.log("height", height, christ);
+
+// 함수
+// 조건문
+// 반복
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+Basic todo-app 만들기!
