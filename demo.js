420797c97895d2807f054c5ab92026e5ca04ccbd
paypassion <zion4596@gmail.com>
2024-03-20 10:26:01 +0900

    5th class
    DOM intro

--- demo.js
+++ demo.js
@@ -22,36 +22,18 @@ undefined;
 height = 180;
 christ = true;
 
-// 저 -객체: 여러값을 가지고 있는 복합적인 데이터 타입
-// -이름
-// -성별
-// -미혼
-// -나이
+// 저
+// - 이름
+// - 성별
+// - 미혼
+// - 나이
 
-me =
-{
-  name: 'lee',
-  gender: 'male',
-  age: 43
-}
-
-// 형식 속성:값
-
-you = {
-  name: 'Kim',
-  gender: 'female',
-  age: 37,
-  address: null,
-  sum: sum
+// 객체
+me = {
+  name: "lee",
+  gender: "male",
+  age: 43,
 };
-
-console.log(you.name);
-console.log(you.age);
-console.log(you.sum(20,20));
-
-
-// 객체(오브젝트)의 프로퍼티에 접근할 때는 점을 쓴다.
-me.age
 
 // 함수
 function sum(x, y) {
@@ -59,19 +41,285 @@ function sum(x, y) {
   return result;
 }
 
+you = {
+  name: "kim",
+  gender: "female",
+  age: 37,
+  address: null,
+  sum: sum,
+};
+
+console.log(you.name);
+console.log(you.age);
+console.log(you.sum(20, 20));
+
 function hi(name) {
-  console.log("hi~"+ name);
+  console.log("hi~ " + name);
 }
 
-시온 = "lonz";
+// 이름 저장
+lastName = "lee";
 
-hi(시온);
+hi(lastName);
 
-sumResult = sum(시온,107);
+sumResult = sum(100, 200);
 
-console.log("sumResult", sumResult);
+console.log(1, sumResult);
+
+console.log();
 
 // 조건문
-// 반복문
+// if(조건) {
+//   실행될 코드
+// }
 
-1단에서 9단까지 구구단 내용을 한꺼번에 쭉 출력하는 콘솔로그 만들어오기
+{
+  let name = "lee";
+
+  if (name === "lee") {
+    console.log("hello");
+  } else if (name === "kim") {
+    console.log("hello");
+  } else {
+    console.log("hi");
+  }
+
+  // switch (변수) {
+  //   case 1:
+  //     console.log("1");
+  //     break;
+  // }
+}
+{
+  let name = "kim";
+
+  switch (name) {
+    case "lee":
+    case "kim":
+      console.log("hello");
+      break;
+    default:
+      console.log("hi");
+  }
+}
+// 변수 선언하는 방법
+
+var name = "park";
+
+var num = 3;
+
+function sum(a, b) {
+  let result = 0;
+  if (true) {
+    const num = 2;
+  }
+  result = (a + b) * num;
+  return result;
+}
+
+// result = sum(1, 1);
+
+// console.log(result);
+// console.log(num);
+
+// 반복문
+// 구구단
+// for(초기값; 범위; 증감연산;) {
+//   반복할 코드
+// }
+
+for (let i = 2; i <= 9; i++) {
+  for (let j = 1; j <= 9; j++) {
+    console.log(i + " x " + j + " = " + i * j);
+  }
+}
+
+// 배열
+const A = [2, 3, 4, 5, 6, 7, 8, 9];
+const B = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(A);
+console.log(A.length);
+console.log(A[0]);
+console.log(A[A.length - 1]);
+
+console.log(Array.prototype);
+
+const AA = [2, 3, 4, 5, 6, 7, 8, 9];
+const result = [];
+
+function iter(console) {
+  if (console % 2 === 0) {
+    result.push(console);
+  }
+}
+
+AA.forEach(iter);
+
+function fa(e) {
+  if (e % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function fb(e) {
+  if (e % 4 === 0) return true;
+  return false;
+}
+
+const r = A.filter(fa).filter(fb);
+
+console.log("r", r);
+
+const products = [
+  {
+    category: 1,
+    name: "바지",
+  },
+  {
+    category: 2,
+    name: "아이스크립",
+  },
+  {
+    category: 2,
+    name: "빵",
+  },
+  {
+    category: 1,
+    name: "상의",
+  },
+  {
+    category: 1,
+    name: "반바지",
+  },
+  {
+    category: 3,
+    name: "목걸이",
+  },
+  {
+    category: 3,
+    name: "반지",
+  },
+  {
+    category: 4,
+    name: "강아지",
+  },
+];
+
+const selectedOption = 3;
+
+function getSelectedProduct1(product) {
+  return product.category === selectedOption;
+}
+
+const getSelectedProduct2 = (product) => {
+  return product.category === selectedOption;
+};
+
+const list = products
+  .filter((product) => product.category === selectedOption)
+  .filter((product) => product.name === "반지");
+
+console.log("list", list);
+
+const products2 = [
+  {
+    category: 1,
+    name: "바지",
+  },
+  {
+    category: 2,
+    name: "아이스크립",
+  },
+  {
+    category: 2,
+    name: "빵",
+  },
+  {
+    category: 1,
+    name: "상의",
+  },
+  {
+    category: 1,
+    name: "반바지",
+  },
+  {
+    category: 3,
+    name: "목걸이",
+  },
+  {
+    category: 3,
+    name: "반지",
+  },
+  {
+    category: 4,
+    name: "강아지",
+  },
+];
+
+function getName(product) {
+  return product.name;
+}
+
+const rr = products2.map(getName);
+
+console.log("rr", rr);
+
+function name(nm) {
+  return nm + " bye";
+}
+
+// console.log(name("lee"));
+
+const studentScore = [
+  {
+    name: "lee",
+    score: 90,
+  },
+  {
+    name: "kim",
+    score: 80,
+  },
+  {
+    name: "park",
+    score: 70,
+  },
+  {
+    name: "choi",
+    score: 40,
+  },
+];
+const pass = [];
+studentScore.forEach((student) => {
+  if (student.score >= 80) {
+    pass.push(student.name);
+  }
+});
+console.log(pass);
+const filteredStudent = studentScore.filter((student) => {
+  return student.score >= 80;
+});
+console.log(filteredStudent);
+
+const passedStudents = filteredStudent.map((student) => {
+  return student.name;
+});
+
+console.log(passedStudents); // ==> ['lee', 'kim']
+
+const passedStudentsByReduce = studentScore.reduce((acc, student) => {
+  if (student.score >= 80) {
+    acc.push(student.name);
+  }
+  return acc;
+}, []);
+
+console.log(passedStudentsByReduce); // ==> ['lee','kim']
+
+const scores = [90, 80, 60, 30, 20];
+const scoreTotal = scores.reduce((sum, score) => {
+  return sum + score;
+}, 0);
+console.log(scoreTotal); // ==> 280
--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log(console);
+//인풋에 내용을 쓰고 등록버튼을 클릭하면 
+//리스트에 내용이 추가된다.
+
+
+const todoInput = document.querySelector('.todo-input');
+const todoInputButton = document.querySelector('.todo-input-button');
+const todoList = document.querySelector('#todoList');
+
+
+console.log("todo", todoInput);
+console.log("todoInputButton",todoInputButton);
+
+todoInputButton.addEventListener('click', function () {
+  const todo = todoInput.value;
+  console.log("todo", todo);
+  
+// 할 일 요소 만드기
+const span = document.createElement("span")
+// 할일리스트에 추가하기
+// todoList => <ul>
+span.textContent = todo;
+
+todoList.appendChild(span);
+})
