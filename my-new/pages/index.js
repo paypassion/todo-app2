a9a45bbb92b95bd63bcf34a830d1c834d67b897c
paypassion <zion4596@gmail.com>
2024-04-11 11:03:36 +0900

    first react

---
+++ my-new/pages/index.js
@@ -0,0 +1,16 @@
+
+function Name() {
+  return <div>장시온</div>
+};
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <Name />
+        <Name />
+        <Name />
+      </div>
+    </>
+  );
+}
